1f25a4dfac8270c59475bb0a7ea4473447d283ab
Johan Mickos <johan.mickos@gmail.com>
2018-08-07 10:38:37 +0200

    Revamp bitfields into slice-based extension

--- src/lib.rs
+++ src/lib.rs
@@ -1,298 +1,407 @@
 //! Helpers for grouping together data in sub-byte bitfields.
 #![feature(try_from)]
 
-use std::collections::HashMap;
 use std::convert::TryFrom;
-use std::mem;
+use std::fmt::Debug;
 
-pub type StorageType = u32;
+type Pos = usize;
+type Width = usize;
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
-    DataTooLarge,
-    OutOfBounds,
-    WouldOverlap,
-    TryFromError,
+    Overflow,
+    TryFromErr,
 }
 
-#[derive(Debug)]
-struct BitField {
-    pos: StorageType,
-    width: StorageType,
-}
-/// A set of bit fields
-#[derive(Debug)]
-pub struct BitFieldSet {
-    /// Total number of bits spanned by this set
-    num_bits: u32,
-    storage: StorageType, // TODO support wider types
-    entries: HashMap<u32, BitField>,
+// TODO  support &[u8]
+/// Helper trait for defining where in the [BitFieldSet] to put data.
+///
+/// # Panics
+/// Currently, the `WIDTH` field is used in the internal bit-shifting on bytes, meaning
+/// that it will panic if the `WIDTH` > 8.
+pub trait BitField<Repr: Sized = u8>: Debug {
+    const POS: Pos;
+    const WIDTH: Width;
 }
 
-impl BitFieldSet {
-    /// Creates a new [BitFieldSet] supporting at most `num_bits` internal bits.
-    pub fn new(num_bits: u32) -> Result<Self, Error> {
-        let supported_bits = (mem::size_of::<StorageType>() * 8) as u32;
-        if num_bits > supported_bits {
-            return Err(Error::OutOfBounds);
-        }
-        Ok(BitFieldSet {
-            num_bits,
-            storage: 0,
-            entries: HashMap::new(),
-        })
-    }
+pub trait BitFieldExt {
+    fn store<Field>(&mut self, field: Field) -> Result<(), Error>
+    where
+        Field: BitField<u8> + Into<u8>;
+    fn get_as<Field>(&self) -> Result<Field, Error>
+    where
+        Field: BitField<u8> + TryFrom<u8>;
+    fn check_field<Field>(&self) -> Result<(), Error>
+    where
+        Field: BitField<u8>;
+}
 
-    /// Creates an associative [BitField] entry in this [BitFieldSet]
-    pub fn add(&mut self, pos: u32, width: u32) -> Result<(), Error> {
-        // TODO leverage same OOB checks as `insert`
-        if pos > self.num_bits {
-            return Err(Error::OutOfBounds);
-        }
-        self.check_overflow(width, pos)?;
-        if self.entries.contains_key(&pos) {
-            return Err(Error::WouldOverlap);
-        }
-        self.entries.insert(pos, BitField { pos, width });
+impl BitFieldExt for [u8] {
+    fn store<Field>(&mut self, field: Field) -> Result<(), Error>
+    where
+        Field: BitField<u8> + Into<u8>,
+    {
+        use bit_twiddles::*;
+
+        self.check_field::<Field>()?;
+
+        let pos = Field::POS;
+        let width = Field::WIDTH;
+
+        let repr: u8 = field.into();
+        (0..width).for_each(|i| {
+            let idx = pos + i;
+            let (byte, bit) = (idx / 8, idx % 8);
+            set_bit_to(&mut self[byte], bit, get_bit(&repr, i));
+        });
         Ok(())
     }
 
-    /// Inserts the the data at the provided position and associates its position and width.
-    pub fn insert<D: Into<StorageType>>(
-        &mut self,
-        pos: u32,
-        width: u32,
-        data: D,
-    ) -> Result<StorageType, Error> {
-        if pos > self.num_bits as StorageType {
-            return Err(Error::OutOfBounds);
+    fn get_as<Field>(&self) -> Result<Field, Error>
+    where
+        Field: BitField<u8> + TryFrom<u8>,
+    {
+        use bit_twiddles::*;
+
+        self.check_field::<Field>()?;
+
+        let pos = Field::POS;
+        let width = Field::WIDTH;
+
+        let mut repr = 0_u8;
+        (0..width).for_each(|i| {
+            let idx = pos + i;
+            let byte = idx / 8;
+            let bit = idx % 8;
+            set_bit_to(&mut repr, i, get_bit(&self[byte], bit));
+        });
+
+        Field::try_from(repr).map_err(|_| Error::TryFromErr)
+    }
+
+    fn check_field<Field>(&self) -> Result<(), Error>
+    where
+        Field: BitField<u8>,
+    {
+        use bit_twiddles::BITS_PER_BYTE;
+
+        if Field::WIDTH > BITS_PER_BYTE {
+            return Err(Error::Overflow);
         }
-        let data: StorageType = data.into();
-        let data_too_large = (mem::size_of::<D>() as u32) > self.num_bits;
-        self.check_overflow(pos, width)?;
-        if data_too_large {
-            return Err(Error::DataTooLarge);
-        }
-        self.storage |= data << pos;
-        self.entries.insert(pos, BitField { pos, width });
-        Ok(data)
-    }
-
-    pub fn get(&self, pos: StorageType) -> Option<StorageType> {
-        let entry = self.entries.get(&pos)?;
-        let mask = (2 as StorageType).pow(entry.width) - 1;
-        let mask = mask << entry.pos;
-        let value = self.storage & mask;
-        let value = value >> entry.pos;
-        Some(value)
-    }
-
-    pub fn get_as<T: TryFrom<StorageType>>(&self, pos: StorageType) -> Result<T, Error> {
-        let value = self.get(pos).ok_or_else(|| Error::TryFromError)?;
-        T::try_from(value).map_err(|_| Error::TryFromError)
-    }
-
-    pub fn get_raw(&self) -> StorageType {
-        self.storage
-    }
-
-    fn check_overflow(&self, width: u32, pos: u32) -> Result<(), Error> {
-        if (width + pos) > self.num_bits {
-            Err(Error::OutOfBounds)
+        let supported_bits = BITS_PER_BYTE * self.len();
+        if (Field::POS + Field::WIDTH) > supported_bits {
+            return Err(Error::Overflow);
         } else {
-            Ok(())
+            return Ok(());
         }
     }
 }
 
-impl From<StorageType> for BitFieldSet {
-    fn from(raw: StorageType) -> Self {
-        let supported_bits = (mem::size_of::<StorageType>() * 8) as u32;
-        BitFieldSet {
-            num_bits: supported_bits,
-            storage: raw,
-            entries: HashMap::new(),
+/// Bit-twiddling helpers
+///
+/// # Panics
+/// All of these will panic if the `pos` parameter exceeds 7
+mod bit_twiddles {
+    pub const BITS_PER_BYTE: usize = 8;
+
+    pub fn byte_bit_offset(idx: usize) -> (usize, usize) {
+        (idx / BITS_PER_BYTE, idx % BITS_PER_BYTE)
+    }
+
+    pub fn get_bit(target: &u8, pos: usize) -> u8 {
+        ((target >> pos) & 0b1)
+    }
+
+    pub fn test_bit(target: &u8, pos: usize) -> bool {
+        get_bit(target, pos) == 0b01
+    }
+
+    pub fn set_bit(target: &mut u8, pos: usize) {
+        *target |= 0b1 << pos;
+    }
+
+    pub fn set_bit_to(target: &mut u8, pos: usize, val: u8) {
+        *target = (*target & !(1 << pos)) | (val << pos);
+    }
+
+    pub fn unset_bit(target: &mut u8, pos: usize) {
+        *target &= !(0b1 << pos);
+    }
+
+    pub fn toggle_bit(target: &mut u8, pos: usize) {
+        *target ^= 0b1 << pos;
+    }
+
+    #[cfg(test)]
+    mod twiddle_tests {
+        use super::*;
+
+        #[test]
+        fn get() {
+            let act: u8 = 0b1010;
+            assert_eq!(get_bit(&act, 0), 0b0);
+            assert_eq!(get_bit(&act, 1), 0b1);
+            assert_eq!(get_bit(&act, 2), 0b0);
+            assert_eq!(get_bit(&act, 3), 0b1);
+        }
+
+        #[test]
+        fn test() {
+            let act: u8 = 0b0001;
+            assert!(test_bit(&act, 0));
+            assert!(!test_bit(&act, 1));
+            assert!(!test_bit(&act, 2));
+            assert!(!test_bit(&act, 3));
+        }
+
+        #[test]
+        fn set() {
+            let mut act: u8 = 0b0000;
+            set_bit(&mut act, 0);
+            assert_eq!(act, 0b0001);
+
+            set_bit(&mut act, 1);
+            assert_eq!(act, 0b0011);
+
+            set_bit(&mut act, 2);
+            assert_eq!(act, 0b0111);
+
+            set_bit(&mut act, 3);
+            assert_eq!(act, 0b1111);
+        }
+
+        #[test]
+        fn set_to() {
+            let mut act: u8 = 0b0000;
+
+            set_bit_to(&mut act, 0, 1);
+            assert_eq!(act, 0b0001);
+            set_bit_to(&mut act, 0, 0);
+            assert_eq!(act, 0b0000);
+
+            set_bit_to(&mut act, 1, 1);
+            assert_eq!(act, 0b0010);
+            set_bit_to(&mut act, 1, 0);
+            assert_eq!(act, 0b0000);
+
+            set_bit_to(&mut act, 2, 1);
+            assert_eq!(act, 0b0100);
+            set_bit_to(&mut act, 2, 0);
+            assert_eq!(act, 0b0000);
+
+            set_bit_to(&mut act, 3, 1);
+            assert_eq!(act, 0b1000);
+            set_bit_to(&mut act, 3, 0);
+            assert_eq!(act, 0b0000);
+        }
+
+        #[test]
+        fn unset() {
+            let mut act: u8 = 0b1111;
+
+            unset_bit(&mut act, 0);
+            assert_eq!(act, 0b1110);
+
+            unset_bit(&mut act, 1);
+            assert_eq!(act, 0b1100);
+
+            unset_bit(&mut act, 2);
+            assert_eq!(act, 0b1000);
+
+            unset_bit(&mut act, 3);
+            assert_eq!(act, 0b0000);
+        }
+
+        #[test]
+        fn toggle() {
+            let mut act: u8 = 0b1010;
+
+            toggle_bit(&mut act, 0);
+            assert_eq!(act, 0b1011);
+
+            toggle_bit(&mut act, 1);
+            assert_eq!(act, 0b1001);
+
+            toggle_bit(&mut act, 2);
+            assert_eq!(act, 0b1101);
+
+            toggle_bit(&mut act, 3);
+            assert_eq!(act, 0b0101);
+        }
+
+        const TOO_LARGE_POS: usize = 10;
+
+        #[test]
+        #[should_panic]
+        fn get_with_large_pos() {
+            let act: u8 = 0b1010;
+            get_bit(&act, TOO_LARGE_POS);
+        }
+
+        #[test]
+        #[should_panic]
+        fn set_with_large_pos() {
+            let mut act: u8 = 0b1010;
+            set_bit(&mut act, TOO_LARGE_POS);
+        }
+
+        #[test]
+        #[should_panic]
+        fn set_to_with_large_pos() {
+            let mut act: u8 = 0b1010;
+            set_bit_to(&mut act, TOO_LARGE_POS, 0b1);
+        }
+
+        #[test]
+        #[should_panic]
+        fn unset_with_large_pos() {
+            let mut act: u8 = 0b1010;
+            unset_bit(&mut act, TOO_LARGE_POS);
+        }
+
+        #[test]
+        #[should_panic]
+        fn toggle_with_large_pos() {
+            let mut act: u8 = 0b1010;
+            toggle_bit(&mut act, TOO_LARGE_POS);
         }
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{BitFieldSet, Error};
-    use std::convert::TryFrom;
-    use StorageType;
-
-    const PATH_TYPE_POS: StorageType = 7;
-    const PROTOCOL_POS: StorageType = 2;
-    const ADDRESS_TYPE_POS: StorageType = 0;
-    const RAW_STORAGE: StorageType = 0b10001001;
+    use super::*;
 
     #[derive(Debug, PartialEq)]
     #[repr(u8)]
-    enum PathTypes {
-        Named,
-        Unique,
+    enum Transport {
+        TCP = 0b01,
+        UDP = 0b10,
+        UDT = 0b11,
     }
 
-    #[derive(Debug, PartialEq)]
-    #[repr(u8)]
-    enum AddressTypes {
-        IPv4,
-        IPv6,
-        Domain,
+    impl BitField for Transport {
+        const POS: usize = 0;
+        const WIDTH: usize = 2;
     }
 
-    #[derive(Debug, PartialEq)]
-    #[repr(u8)]
-    enum ProtocolTypes {
-        Local,
-        TCP,
-        UDP,
-        UDT,
+    impl Into<u8> for Transport {
+        fn into(self) -> u8 {
+            self as u8
+        }
     }
 
-    impl TryFrom<StorageType> for PathTypes {
-        type Error = Error;
+    impl TryFrom<u8> for Transport {
+        type Error = ();
 
-        fn try_from(value: StorageType) -> Result<Self, Self::Error> {
+        fn try_from(value: u8) -> Result<Self, Self::Error> {
             match value {
-                x if x == PathTypes::Named as StorageType => Ok(PathTypes::Named),
-                x if x == PathTypes::Unique as StorageType => Ok(PathTypes::Unique),
-                _other => Err(Error::TryFromError),
+                0b01 => Ok(Transport::TCP),
+                0b10 => Ok(Transport::UDP),
+                0b11 => Ok(Transport::UDT),
+                _ => Err(()),
             }
         }
     }
 
-    impl TryFrom<StorageType> for AddressTypes {
-        type Error = Error;
+    #[derive(Debug, PartialEq)]
+    #[repr(u8)]
+    enum WideWithOffset {
+        A = 0b1111_1000,
+        B = 0b1111_1100,
+        C = 0b1111_1110,
+        D = 0b1111_1111,
+    }
 
-        fn try_from(value: StorageType) -> Result<Self, Self::Error> {
+    impl BitField for WideWithOffset {
+        const POS: usize = 7;
+        const WIDTH: usize = 2;
+    }
+
+    impl Into<u8> for WideWithOffset {
+        fn into(self) -> u8 {
+            self as u8
+        }
+    }
+
+    impl TryFrom<u8> for WideWithOffset {
+        type Error = ();
+
+        fn try_from(value: u8) -> Result<Self, Self::Error> {
             match value {
-                x if x == AddressTypes::IPv4 as StorageType => Ok(AddressTypes::IPv4),
-                x if x == AddressTypes::IPv6 as StorageType => Ok(AddressTypes::IPv6),
-                x if x == AddressTypes::Domain as StorageType => Ok(AddressTypes::Domain),
-                _other => Err(Error::TryFromError),
+                0b1111_1000 => Ok(WideWithOffset::A),
+                0b1111_1100 => Ok(WideWithOffset::B),
+                0b1111_1110 => Ok(WideWithOffset::C),
+                0b1111_1111 => Ok(WideWithOffset::D),
+                _ => Err(()),
             }
         }
     }
 
-    impl TryFrom<StorageType> for ProtocolTypes {
-        type Error = Error;
+    #[derive(Debug, PartialEq)]
+    #[repr(u8)]
+    enum InvalidWidth {
+        DoNotCare = 0b0,
+    }
 
-        fn try_from(value: StorageType) -> Result<Self, Self::Error> {
+    impl BitField for InvalidWidth {
+        const POS: usize = 0;
+        const WIDTH: usize = 9; // exceeds current allowed width
+    }
+
+    impl Into<u8> for InvalidWidth {
+        fn into(self) -> u8 {
+            self as u8
+        }
+    }
+
+    impl TryFrom<u8> for InvalidWidth {
+        type Error = ();
+
+        fn try_from(value: u8) -> Result<Self, Self::Error> {
             match value {
-                x if x == ProtocolTypes::Local as StorageType => Ok(ProtocolTypes::Local),
-                x if x == ProtocolTypes::TCP as StorageType => Ok(ProtocolTypes::TCP),
-                x if x == ProtocolTypes::UDP as StorageType => Ok(ProtocolTypes::UDP),
-                x if x == ProtocolTypes::UDT as StorageType => Ok(ProtocolTypes::UDT),
-                _other => Err(Error::TryFromError),
+                0b0 => Ok(InvalidWidth::DoNotCare),
+                _ => Err(()),
             }
         }
     }
 
     #[test]
-    fn insertion() {
-        // TODO force compiler-aware mapping of position to type stored
-        let mut bfs = BitFieldSet::new(8).expect("8 bits should fit into default storage type u32");
-        bfs.insert(PATH_TYPE_POS, 1, PathTypes::Unique as u8)
-            .expect("Data width of 1 should fit inside expected 32 bits");
-        bfs.insert(PROTOCOL_POS, 5, ProtocolTypes::UDP as u8)
-            .expect("Data width of 5 should fit inside expected 32 bits");
-        bfs.insert(ADDRESS_TYPE_POS, 2, AddressTypes::IPv6 as u8)
-            .expect("Data width of 2 should fit inside expected 32 bits");
-
-        assert_eq!(
-            bfs.get(PATH_TYPE_POS).unwrap(),
-            PathTypes::Unique as StorageType
-        );
-        assert_eq!(
-            bfs.get_as::<PathTypes>(PATH_TYPE_POS).unwrap(),
-            PathTypes::Unique
-        );
-        assert_eq!(
-            bfs.get_as::<AddressTypes>(ADDRESS_TYPE_POS).unwrap(),
-            AddressTypes::IPv6
-        );
-        assert_eq!(
-            bfs.get_as::<ProtocolTypes>(PROTOCOL_POS).unwrap(),
-            ProtocolTypes::UDP
-        );
+    fn store_and_retrieve() {
+        use super::BitFieldExt;
+        let mut storage = vec![0u8, 0u8];
+        storage.store(Transport::TCP).unwrap();
+        assert_eq!(storage.get_as::<Transport>().unwrap(), Transport::TCP);
     }
 
     #[test]
-    fn from_raw() {
-        let mut bfs = BitFieldSet::from(RAW_STORAGE);
-        bfs.add(PATH_TYPE_POS, 1)
-            .expect("Data of width 1 should fit inside expected 32 bits");
-        bfs.add(PROTOCOL_POS, 5)
-            .expect("Data of width 1 should fit inside expected 32 bits");
-        bfs.add(ADDRESS_TYPE_POS, 2)
-            .expect("Data of width 1 should fit inside expected 32 bits");
-
-        assert_eq!(
-            bfs.get(PATH_TYPE_POS).unwrap(),
-            PathTypes::Unique as StorageType
-        );
-        assert_eq!(
-            bfs.get_as::<PathTypes>(PATH_TYPE_POS).unwrap(),
-            PathTypes::Unique
-        );
-        assert_eq!(
-            bfs.get_as::<AddressTypes>(ADDRESS_TYPE_POS).unwrap(),
-            AddressTypes::IPv6
-        );
-        assert_eq!(
-            bfs.get_as::<ProtocolTypes>(PROTOCOL_POS).unwrap(),
-            ProtocolTypes::UDP
-        );
+    fn overwrite() {
+        let mut storage = vec![0u8, 0u8];
+        storage.store(Transport::TCP).unwrap();
+        assert_eq!(storage.get_as::<Transport>().unwrap(), Transport::TCP);
+        storage.store(Transport::UDP).unwrap();
+        assert_eq!(storage.get_as::<Transport>().unwrap(), Transport::UDP);
     }
 
     #[test]
-    fn into_raw() {
-        let mut bfs = BitFieldSet::new(8).unwrap();
-        bfs.insert(PATH_TYPE_POS, 1, PathTypes::Unique as u8)
-            .unwrap();
-        bfs.insert(PROTOCOL_POS, 5, ProtocolTypes::UDP as u8)
-            .unwrap();
-        bfs.insert(ADDRESS_TYPE_POS, 2, AddressTypes::IPv6 as u8)
-            .unwrap();
-        let raw = bfs.get_raw();
-        assert_eq!(
-            raw, RAW_STORAGE,
-            "Inserted data should match hardocded expected bits"
-        );
-    }
-    #[test]
-    fn truncated_raw() {
-        let mut bfs = BitFieldSet::new(8).unwrap();
-        bfs.insert(PATH_TYPE_POS, 1, PathTypes::Unique as u8)
-            .unwrap();
-        bfs.insert(PROTOCOL_POS, 5, ProtocolTypes::UDP as u8)
-            .unwrap();
-        bfs.insert(ADDRESS_TYPE_POS, 2, AddressTypes::IPv6 as u8)
-            .unwrap();
-        let raw = bfs.get_raw() as u8;
-        assert_eq!(
-            raw as u32, RAW_STORAGE,
-            "Inserted data should match hardocded expected bits"
-        );
+    fn store_too_wide() {
+        // small_storage is only 8 bits, and thus cannot fit the WideWithOffset
+        let mut small_storage = vec![0u8];
+        assert_eq!(small_storage.store(WideWithOffset::A), Err(Error::Overflow));
     }
 
     #[test]
-    fn bad_insertion() {
-        let mut bfs = BitFieldSet::new(8).unwrap();
-        // Valid insertion
-        bfs.add(PATH_TYPE_POS, 1).unwrap();
-        // Invalid re-insertion at existing position
-        let res = bfs.add(PATH_TYPE_POS, 1);
-
-        assert_eq!(res, Err(Error::WouldOverlap));
+    fn corrupted_read() {
+        // 0b0000_0000 is not a valid representation for [Transport]
+        let mut storage = vec![0u8];
+        assert_eq!(storage.get_as::<Transport>(), Err(Error::TryFromErr));
     }
 
     #[test]
-    fn out_of_bounds_insertion() {
-        let mut bfs = BitFieldSet::new(8).unwrap();
-        let res = bfs.add(8, 9);
-        assert_eq!(res, Err(Error::OutOfBounds));
+    fn invalid_field() {
+        let mut storage = vec![0u8, 0u8];
+        // `InvalidWidth` has a width of 9, exceeding the current allowed field width
+        assert_eq!(storage.store(InvalidWidth::DoNotCare), Err(Error::Overflow));
+        assert_eq!(storage.get_as::<InvalidWidth>(), Err(Error::Overflow));
     }
 }
